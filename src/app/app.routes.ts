7bf6edd23463d3b4716d3e291f9f2265a2fd1cf6
Fernanda <nanda.kipper@gmail.com>
2024-05-30 13:38:14 -0300

    feat: create post detail page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { PostComponent } from './pages/post/post.component';
 
 export const routes: Routes = [
     {
         path: "",
         component: HomeComponent
+    },
+    {
+        path: 'post/:id',
+        component: PostComponent
     }
 ];
